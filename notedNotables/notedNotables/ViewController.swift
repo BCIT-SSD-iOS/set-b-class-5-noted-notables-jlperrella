d1e019e57ff304975980749aee3ed1379f06fd40
jp <jlperrella@gmail.com>
2019-03-13 10:10:16 -0700

    project files created

---
+++ notedNotables/notedNotables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notedNotables
+//
+//  Created by jp on 2019-03-13.
+//  Copyright © 2019 Jordan Perrella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
